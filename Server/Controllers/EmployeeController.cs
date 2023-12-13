2a6ef663138d1e093703568ddd2cb81963dc66b9
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-12-13 11:40:19 +0000

    Added EmployeeController API Controller class

---
+++ Server/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using blazor_wasm_sql_db.Client.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blazor_wasm_sql_db.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public async Task<ActionResult<List<Employee>>> GetAllEmployees()
+        {
+            var list = new List<Employee>
+            { 
+                new Employee
+                {
+                    Id = 1,
+                    Code = "HR1",
+                    FullName = "Joe Bloggs",
+                    Address = "123 New Street",
+                    JoiningDate = new DateTime(2023, 12, 13)
+                }
+            };
+
+            return Ok(list);
+        }
+    }
+}
