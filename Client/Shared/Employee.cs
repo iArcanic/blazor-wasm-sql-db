85aea40b131a03f80cc83586ff8873a8f4c1e4ec
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-12-13 11:33:12 +0000

    Added Employee class and configured network ports for runtime

---
+++ Client/Shared/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blazor_wasm_sql_db.Client.Shared
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        public string? Address { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime JoiningDate { get; set; }
+    }
+}
