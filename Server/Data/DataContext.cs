e65bd8eaa39c1bde022986211cb9ae9ade6977a1
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-12-13 12:09:51 +0000

    Added DataContext class

---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using blazor_wasm_sql_db.Client.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace blazor_was_sql_db.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
