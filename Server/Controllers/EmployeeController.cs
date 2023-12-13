c78f97c82f58ca25283b1c866415a762ebf51a2a
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-12-13 16:42:00 +0000

    Added more HTTP methods for editing functionality

--- Server/Controllers/EmployeeController.cs
+++ Server/Controllers/EmployeeController.cs
@@ -23,10 +23,45 @@ namespace blazor_wasm_sql_db.Server.Controllers
             return Ok(await _context.Employees.ToListAsync());
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Employee>>> GetEmployees(int id)
+        {
+            var dbEmployee = await _context.Employees.FindAsync(id);
+
+            if (dbEmployee == null)
+            {
+                return NotFound("This Employee does not exist!");
+            }
+            
+            return Ok(dbEmployee);
+        }
+
         [HttpPost]
         public async Task<ActionResult<List<Employee>>> AddEmployees(Employee employee)
         {
             _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return await GetAllEmployees();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Employee>>> UpdateEmployees(int id, Employee employee)
+        {
+            var dbEmployee = await _context.Employees.FindAsync(id);
+
+            if (dbEmployee == null)
+            {
+                return NotFound("This Employee does not exist!");
+            }
+
+            dbEmployee.Id = employee.Id;
+            dbEmployee.Code = employee.Code;
+            dbEmployee.FullName = employee.FullName;
+            dbEmployee.Address = employee.Address;
+            dbEmployee.JoiningDate = employee.JoiningDate;
+
+            _context.Add(dbEmployee);
             await _context.SaveChangesAsync();
 
             return await GetAllEmployees();
